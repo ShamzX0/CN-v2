3a1ed5ed14e04914fa56db25ee9ea38063f826cb
ShamzX0 <posp.petrr@gmail.com>
2025-02-20 15:00:03 +1300

    minor CSS styling chage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Main from '@/components/Main/Main'
 export default function Home() {
 
   return (
-    <main className="rounded-xl min-h-screen">
+    <main className="rounded-xl flex-1">
       <Main />
     </main>
   )
