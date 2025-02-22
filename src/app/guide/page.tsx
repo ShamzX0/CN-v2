a7083bf06086bf1452d4d2645f0482df2f2172dc
ShamzX0 <posp.petrr@gmail.com>
2025-02-22 16:14:33 +1300

    tidy: guide page

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -2,12 +2,10 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import btcImage from '../../../public/images/btcImg.jpeg'
+import btcImage from '../../../public/images/btcImg.jpeg';
 import CryptoCards from '@/components/Main/Articles/CryptoCards';
 
-
-
-const HeaderSection = ({ title, subtitle, imageSrc }: { title: string; subtitle: string; imageSrc?: any }) => (
+const HeaderSection = ({ title, subtitle, imageSrc }: { title: string, subtitle: string, imageSrc?: string }) => (
     <section className="mb-8">
         <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl text-white">
             {title}
@@ -18,12 +16,10 @@ const HeaderSection = ({ title, subtitle, imageSrc }: { title: string; subtitle:
     </section>
 );
 
-
 const Page = () => {
     return (
         <main className="container w-full md:max-w-4xl mx-auto py-10 text-[#cccbcb]">
             <div className="w-full mt-10 px-4 text-l leading-normal">
-                {/* Header Section */}
                 <HeaderSection
                     title="What is Cryptocurrency?"
                     subtitle="The Digital Currency Revolution"
@@ -31,65 +27,45 @@ const Page = () => {
                 />
                 <p className="text-lg font-normal mt-3 mb-8 lg:text-xl text-gray-400">
                     In the ever-evolving landscape of digital finance, cryptocurrencies have emerged as a disruptive force,
-                    reshaping the way we perceive and transact value. We recognize the immense potential of this digital currency
-                    revolution, where technology and innovation are poised to change the world.
+                    reshaping the way we perceive and transact value. This revolution, where technology and innovation converge,
+                    is set to change the world.
                 </p>
-
-                {/* Main Content */}
                 <p className="mb-3 text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-[#208282] first-letter:mr-1 first-letter:float-left">
-                    Cryptocurrency is such a cryptic concept, isn&apos;t it? Many people have difficulty understanding the
-                    technology that powers cryptocurrency, let alone how it works as an investment. Over the past decade, the
-                    worth of cryptocurrency has skyrocketed beyond many investor&apos;s expectations. We intend to shed light on
-                    this concept.
+                    Cryptocurrency is a complex concept. Many people struggle to understand the technology behind it, let alone
+                    its investment potential. Over the past decade, cryptocurrency values have soared, surprising even the most
+                    skeptical investors. This guide aims to demystify the subject.
                 </p>
                 <p className="text-lg font-normal mt-3 mb-3 lg:text-xl text-gray-400">
-                    This beginner&apos;s guide will provide you with a solid foundation in understanding cryptocurrencies,
-                    blockchain technology, and the distinctions between various projects and blockchains. Armed with this
-                    knowledge, you&apos;ll be empowered to contemplate which specific cryptocurrency or blockchain aligns best
-                    with your interests and goals.
+                    This beginner's guide will introduce you to cryptocurrencies, blockchain technology, and the distinctions between
+                    different projects. With this knowledge, you can decide which cryptocurrency aligns with your goals.
                 </p>
-                <div className="text-4xl font-extrabold leading-none tracking-tight md:text-4xl lg:text-4xl text-white">
-                    <h5 className=" underline mt-12 underline-offset-3 decoration-8 decoration-[#1d7272]">
-                        Brief Introduction
-                    </h5>
-                </div>
-
-                {/* Subsections */}
+                <h5 className="underline mt-12 underline-offset-3 decoration-8 decoration-[#1d7272] text-4xl font-extrabold">
+                    Brief Introduction
+                </h5>
                 <p className="py-6">
-                    Cryptocurrency has emerged as a disruptive force in the world of finance and technology. It represents a new
-                    form of digital currency that operates independently of traditional banking systems. In this article, we will
-                    delve into the concept of cryptocurrency, its underlying technology, and its potential implications.
+                    Cryptocurrency is revolutionizing finance and technology. Operating independently of traditional banking,
+                    it represents a new form of digital currency. This article explores cryptocurrency's foundation, technology,
+                    and potential implications.
                 </p>
                 <div className='py-6'>
                     <div className="border-b-2 border-[#1d7272]" />
-                    <h4 className="border-l-7 border-gray-500 font-medium text-lg italic my-8 neon-writing ">
-                        Key Characteristics that you should know about..
+                    <h4 className="border-l-7 border-gray-500 font-medium text-lg italic my-8 neon-writing">
+                        Key Characteristics You Should Know About...
                     </h4>
                     <div className="border-b-[1px] border-[#1d7272]" />
                 </div>
-
-                {/* Types of Cryptocurrencies */}
-                <h5 className="my-10 text-4xl font-extrabold leading-none tracking-tight md:text-4xl lg:text-4xl text-white">
+                <h5 className="my-10 text-4xl font-extrabold tracking-tight text-white">
                     <span className="underline underline-offset-3 decoration-8 decoration-[#1d7272]">Types of Cryptocurrencies</span>
                 </h5>
                 <p className="text-lg font-normal lg:text-xl text-gray-400">
-                    While Bitcoin reigns as the undisputed king of cryptocurrencies, the digital landscape is home to a vast array
-                    of other digital assets, each with its own distinctive characteristics and objectives. Let us show you a few
-                    exciting curated collection of remarkable cryptocurrencies that we firmly believe possess a solid foundation
-                    and a promising future in the ever-evolving crypto ecosystem.
+                    Bitcoin remains the king of cryptocurrencies, but the digital ecosystem is filled with diverse assets.
+                    Here, we showcase some of the most promising cryptocurrencies with solid foundations and exciting futures.
                 </p>
-
-
                 <CryptoCards />
-
-                {/* Quote Section */}
                 <blockquote className="border-l-4 border-[#1d7272] italic my-8 pl-8 md:pl-12">
-                    Cryptocurrencies have revolutionized the way we perceive and transact value. With their decentralized nature
-                    and blockchain technology, they offer a new paradigm of financial systems that are transparent, secure, and
-                    accessible to all.
+                    Cryptocurrencies have transformed how we perceive and exchange value. Their decentralized nature and blockchain
+                    technology provide transparency, security, and global accessibility.
                 </blockquote>
-
-                {/* Footer Link */}
                 <div className="text-base md:text-sm mt-11 text-gray-500 px-4">
                     <Link
                         href="/"
