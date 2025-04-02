5fc88f892e2cd6d7d952707915753709d0dfb5e9
ShamzX0 <posp.petrr@gmail.com>
2025-04-02 13:26:21 +0700

    tidy

--- src/components/Main/Footer/Footer.tsx
+++ src/components/Main/Footer/Footer.tsx
@@ -10,52 +10,51 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="text-gray-300">
-            <div className="w-full mx-auto py-4">
-                {/* Divider */}
-                <div className="border-[1px] border-transparent border-[#00d9ff] neon-card mb-6" />
-                <div className="flex gap-3 items-center">
-                    <Image src={cnlogo} alt="CryptoNewbieCap logo" width={30} height={10} />
-                    <h1 className="font-extrabold text-sm font-mono">
-                        Crypto<span className="text-[#00FFFF]">N</span>ewbie
-                    </h1>
+        <footer className="w-full mx-auto py-4 text-gray-300">
+            {/* Divider */}
+            <div className="border-[1px] border-transparent border-[#00d9ff] neon-card mb-6" />
+            <div className="flex gap-3 items-center">
+                <Image src={cnlogo} alt="CryptoNewbieCap logo" width={30} height={10} />
+                <h1 className="font-extrabold text-sm font-mono">
+                    Crypto<span className="text-[#00FFFF]">N</span>ewbie
+                </h1>
+            </div>
+            <div>
+                <div className="flex justify-center space-x-6">
+                    <a href="https://www.facebook.com/profile.php?id=1775273260" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                        <FiFacebook className="w-6 h-6 hover:text-[#00FFFF] transition-colors" />
+                    </a>
+                    <a href="https://x.com/_ShamzX_" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+                        <FaXTwitter className="w-6 h-6 hover:text-[#00FFFF] transition-colors" />
+                    </a>
+                    <a href="https://www.instagram.com/ourr.travels/?hl=en" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                        <BsInstagram className="w-6 h-6 hover:text-[#00FFFF] transition-colors" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/petr-mirf%C3%ABa-56a104270/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+                        <SlSocialLinkedin className="w-6 h-6 hover:text-[#00FFFF] transition-colors" />
+                    </a>
+                    <a href="https://github.com/ShamzX0" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+                        <FaGithub className="w-6 h-6 hover:text-[#00FFFF] transition-colors" />
+                    </a>
                 </div>
-                <div>
-                    <div className="flex justify-center space-x-6">
-                        <a href="https://www.facebook.com/profile.php?id=1775273260" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-                            <FiFacebook className="w-6 h-6 hover:text-[#00FFFF] transition-colors" />
-                        </a>
-                        <a href="https://x.com/_ShamzX_" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-                            <FaXTwitter className="w-6 h-6 hover:text-[#00FFFF] transition-colors" />
-                        </a>
-                        <a href="https://www.instagram.com/ourr.travels/?hl=en" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-                            <BsInstagram className="w-6 h-6 hover:text-[#00FFFF] transition-colors" />
-                        </a>
-                        <a href="https://www.linkedin.com/in/petr-mirf%C3%ABa-56a104270/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-                            <SlSocialLinkedin className="w-6 h-6 hover:text-[#00FFFF] transition-colors" />
-                        </a>
-                        <a href="https://github.com/ShamzX0" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-                            <FaGithub className="w-6 h-6 hover:text-[#00FFFF] transition-colors" />
-                        </a>
-                    </div>
 
-                    {/* Bottom bar */}
-                    <div className="flex flex-col md:flex-row justify-between items-center">
-                        <p className="text-xs">
-                            © {currentYear} CryptoNewbie. All rights reserved.
+                {/* Bottom bar */}
+                <div className="flex flex-col md:flex-row justify-between items-center">
+                    <p className="text-xs">
+                        © {currentYear} CryptoNewbie. All rights reserved.
+                    </p>
+                    <div className="flex space-x-4 mt-4 md:mt-0">
+                        <p className="text-xs hover:text-white transition-colors">
+                            Privacy Policy
                         </p>
-                        <div className="flex space-x-4 mt-4 md:mt-0">
-                            <p className="text-xs hover:text-white transition-colors">
-                                Privacy Policy
-                            </p>
-                            <p className="text-xs hover:text-white transition-colors">
-                                Terms of Service
-                            </p>
-                        </div>
+                        <p className="text-xs hover:text-white transition-colors">
+                            Terms of Service
+                        </p>
                     </div>
                 </div>
             </div>
         </footer>
+
     );
 };
 
