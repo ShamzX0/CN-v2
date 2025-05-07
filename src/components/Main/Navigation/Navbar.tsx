0d3ea46e5acc13e5a6d8860892d42cebd7c145e5
ShamzX0 <posp.petrr@gmail.com>
2025-05-07 18:52:18 +0700

    feat: adding selection to Connect wallet for sepolia option

--- src/components/Main/Navigation/Navbar.tsx
+++ src/components/Main/Navigation/Navbar.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useAccount, useChainId, useSwitchChain } from 'wagmi'
 
 // Components
 import NavLinks from '../Navigation/NavLinks'
@@ -12,6 +13,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 // Assets
 import CryptoNewbie from '../../../../public/images/CryptoNewbie.png'
+import { Wallet } from 'lucide-react'
 
 // Styling constants
 const NEON_EFFECTS = {
@@ -29,86 +31,157 @@ const Divider = () => (
     <hr className="h-4 w-px mx-2 bg-[#f4f4f4] opacity-30" />
 );
 
-export const WalletConnect: React.FC<WalletConnectProps> = ({ pathname }) => (
-    <div
-        className={`hover:text-[#61d9f1] scale-[0.9] hover:border-transparent rounded border-slate-600 border-[1px] transition duration-300 ease-in-out
-        ${pathname === '/swap' ? NEON_EFFECTS.active : NEON_EFFECTS.walletHover}`}
-    >
-        <ConnectButton.Custom>
-            {({
-                account,
-                chain,
-                openAccountModal,
-                openConnectModal,
-                mounted,
-            }) => {
-                const ready = mounted;
-                const connected = ready && account && chain;
+export const WalletConnect: React.FC<WalletConnectProps> = ({ pathname }) => {
+    const chainId = useChainId()
+    const { switchChain } = useSwitchChain()
+    const [showNetworkSelect, setShowNetworkSelect] = React.useState(false)
+    const dropdownRef = React.useRef<HTMLDivElement>(null);
+    const iconRef = React.useRef<HTMLDivElement>(null);
 
-                return (
-                    <div
-                        {...(!ready && {
-                            'aria-hidden': true,
-                            style: {
-                                opacity: 0,
-                                pointerEvents: 'none',
-                                userSelect: 'none',
-                            },
-                        })}
-                    >
-                        {(() => {
-                            const baseButtonStyles =
-                                "bg-transparent border-none text-gray-300 hover:scale-[1.03] hover:text-cyan-400 transition duration-300 ease-in-out flex items-center justify-center gap-2 px-4 py-2 ";
+    const handleNetworkSelect = (newChainId: number) => {
+        switchChain?.({ chainId: newChainId })
+        setShowNetworkSelect(false)
+    }
 
-                            if (!connected) {
-                                return (
-                                    <button
-                                        onClick={openConnectModal}
-                                        type="button"
-                                        className={baseButtonStyles}
-                                    >
-                                        Connect Wallet
-                                    </button>
-                                );
-                            }
+    React.useEffect(() => {
+        if (!showNetworkSelect) {
+            return;
+        }
 
-                            return (
-                                <button
-                                    onClick={openAccountModal}
-                                    type="button"
-                                    className={baseButtonStyles}
-                                >
-                                    {/* Icon + Chain Name + Address */}
-                                    <div className="flex items-center gap-2">
-                                        {chain?.iconUrl && typeof chain.iconUrl === 'string' && (
-                                            <Image
-                                                src={chain.iconUrl!}
-                                                alt={chain.name ?? 'Chain icon'}
-                                                width={24}
-                                                height={24}
-                                                className="w-6 h-6"
-                                            />
-                                        )}
-                                        <Divider />
-                                        <span
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target as Node) &&
+                iconRef.current &&
+                !iconRef.current.contains(event.target as Node)
+            ) {
+                setShowNetworkSelect(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [showNetworkSelect]);
+
+    return (
+        <div
+            className={`hover:text-[#61d9f1] scale-[0.9] hover:border-transparent rounded border-slate-600 border-[1px] transition duration-300 ease-in-out
+            ${pathname === '/swap' ? NEON_EFFECTS.active : NEON_EFFECTS.walletHover}`}
+        >
+            <ConnectButton.Custom>
+                {({
+                    account,
+                    chain,
+                    openAccountModal,
+                    openConnectModal,
+                    mounted,
+                }) => {
+                    const ready = mounted;
+                    const connected = ready && account && chain;
+
+                    return (
+                        <div
+                            {...(!ready && {
+                                'aria-hidden': true,
+                                style: {
+                                    opacity: 0,
+                                    pointerEvents: 'none',
+                                    userSelect: 'none',
+                                },
+                            })}
+                        >
+                            {(() => {
+                                const baseButtonStyles =
+                                    "bg-transparent border-none text-gray-300 hover:text-cyan-400 transition duration-300 ease-in-out flex items-center justify-center gap-2 px-4 py-2 ";
+
+                                if (!connected) {
+                                    return (
+                                        <button
                                             onClick={openConnectModal}
-                                            className="text-[#f4f4f4] transition-colors duration-300 hover:text-cyan-400"
+                                            type="button"
+                                            className={baseButtonStyles}
                                         >
-                                            {account.displayName}
-                                        </span>
+                                            <Wallet size={15} /> Connect Wallet
+                                        </button>
+                                    );
+                                }
 
+                                return (
+                                    <div className="relative">
+                                        <button
+                                            onClick={openAccountModal}
+                                            type="button"
+                                            className={baseButtonStyles}
+                                        >
+                                            {/* Icon + Chain Name + Address */}
+                                            <div className="flex items-center gap-2">
+                                                <div
+                                                    ref={iconRef}
+                                                    onClick={(e) => {
+                                                        e.stopPropagation();
+                                                        setShowNetworkSelect(!showNetworkSelect);
+                                                    }}
+                                                    className="cursor-pointer"
+                                                >
+                                                    {chain?.iconUrl && typeof chain.iconUrl === 'string' && (
+                                                        <Image
+                                                            src={chain.iconUrl!}
+                                                            alt={chain.name ?? 'Chain icon'}
+                                                            width={24}
+                                                            height={24}
+                                                            className="w-6 h-6 hover:scale-[1.3] transition-all duration-500 ease-in-out"
+                                                        />
+                                                    )}
+                                                </div>
+                                                <Divider />
+                                                <span
+                                                    onClick={openConnectModal}
+                                                    className="text-[#f4f4f4] transition-colors duration-300 hover:text-cyan-400"
+                                                >
+                                                    {account.displayName}
+                                                </span>
+                                            </div>
+                                        </button>
+
+                                        {showNetworkSelect && (
+                                            <div
+                                                ref={dropdownRef}
+                                                className="absolute top-full w-full left-0 mt-2 bg-gray-800/50 backdrop-blur-lg rounded-lg shadow-lg z-50">
+                                                {[
+                                                    { id: 1, label: ['Ethereum', 'Mainnet'] },
+                                                    { id: 11155111, label: ['Sepolia', 'Testnet'] }
+                                                ].map((net) => {
+                                                    const isActive = chain?.id === net.id;
+                                                    return (
+                                                        <button
+                                                            key={net.id}
+                                                            onClick={() => handleNetworkSelect(net.id)}
+                                                            className={`w-full flex items-center gap-3 px-4 py-4 rounded transition-colors duration-200 font-bold hover:bg-gray-700 ${isActive ? 'text-gray-300 border-b-[1px] border-opacity-50 border-gray-400' : 'text-gray-400'}`}
+                                                        >
+                                                            <span className={`flex items-center justify-center w-7 h-7 rounded-full border-[1px] ${isActive ? 'border-cyan-400' : 'border-gray-400'}`}>
+                                                                <span className={`w-4 h-4 rounded-full block ${isActive ? 'bg-cyan-400' : 'bg-gray-400'}`} />
+                                                            </span>
+                                                            <span className="flex flex-col items-start leading-tight">
+                                                                <span className="text-sm">{net.label[0]}</span>
+                                                                <span className="text-sm">{net.label[1]}</span>
+                                                            </span>
+                                                        </button>
+                                                    );
+                                                })}
+                                            </div>
+                                        )}
                                     </div>
-                                </button>
-                            );
-                        })()}
-                    </div>
-                );
-
-
-            }}
-        </ConnectButton.Custom>
-    </div>
-);
+                                );
+                            })()}
+                        </div>
+                    );
+                }}
+            </ConnectButton.Custom>
+        </div>
+    );
+};
 
 const LaunchAppButton: React.FC = () => (
     <Link
@@ -145,7 +218,7 @@ const Navbar: React.FC = () => {
 
             {/* Launch App btn / Connect wallet btn */}
             <div className='flex flex-row gap-4'>
-                <WalletConnect pathname={pathname} />
+                {pathname === '/swap' && <WalletConnect pathname={pathname} />}
                 {pathname !== '/swap' && <LaunchAppButton />}
             </div>
         </nav>
